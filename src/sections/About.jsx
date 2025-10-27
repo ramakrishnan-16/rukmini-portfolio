2ef561ec36258560b0fd1d29e12dc8556f9c5586
ramakrishnan_16 <ramakrishnan.j16@gmail.com>
2025-10-27 14:41:04 +0530

    updated portfolio

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -7,10 +7,10 @@ const About = () => {
       <div className="container about-container p-5 mt-5">
         <h2 className="about-heading mb-4 text-center fw-bold">About Me</h2>
         <p className="about-text">
-          I am Rukmini, destined for a creative journey. I graduated with a bachelor's degree in History from Maharajas College in Ernakulam and completed a Postgraduate Diploma in Television Journalism from Kerala Media Academy in Kakkanad.
+          I am Rukmini, graduated with a bachelor's degree in History from Maharajas College in Ernakulam and a Postgraduate diploma in Television Journalism from Kerala Media Academy in Kakkanad.  I give credit to my passion for art.  
         </p>
         <p className="about-text">
-          I write creatively, including short verses, blogs, and stories. I started working on TVCs and social media advertisements as an Assistant Art Director and currently work as a Copywriter for a Kochi-based digital marketing company.
+          I write creatively, short verses, blogs, stories.  Began working on TVCs and social media advertisements as an Assistant Art Director. Also working as a Copywriter for a Kochi-based digital marketing company.  Still figuring out the artistic pursuit...
         </p>
         <div className="d-flex justify-content-center mt-4">
           <a href="#contact" className="btn btn-danger about-btn">Contact Me</a>
